67a341eab33be3f551fea30f2df9890ffeb38696
onxzy <contact@onxzy.dev>
2022-12-04 22:27:01 +0100

    client url

--- src/app/app.js
+++ src/app/app.js
@@ -14,18 +14,16 @@ app.use('/:link_id', (req, res) => {
   console.log(req.params.link_id);
   databases.listDocuments(process.env.APPWRITE_DB, process.env.APPWRITE_COLLECTION, [Query.equal('short', req.params.link_id)])
     .then((link) => {
-      if (link.total == 0) res.redirect('https://l.onxzy.dev/');
+      if (link.total == 0) res.redirect('https://links.onxzy.dev/');
       else res.redirect(link.documents[0].dest);
     })
     .catch((err) => {
-      if (err.code == 404) res.redirect('https://l.onxzy.dev/');
+      if (err.code == 404) res.redirect('https://links.onxzy.dev/');
       else {
         console.error(err);
         res.status(500).send();
       }
     })
- 
-  
 })
 app.use('/', (req, res) => res.status(404).send());
 
